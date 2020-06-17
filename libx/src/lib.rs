ec9dbe5105a54759ceb221aad7359b1a277cb792
Maximilian Schulke <schulkemaximilian@gmail.com>
2020-06-17 14:26:12 +0200

    docs: add notes about implementation plan for libx

--- libx/src/lib.rs
+++ libx/src/lib.rs
@@ -1,3 +1,6 @@
+// this lib will contain easy bindings for creating, moving and closing x windows
+// probably using the following crate: https://docs.rs/x11rb/0.5.0/x11rb/
+
 #[cfg(test)]
 mod tests {
     #[test]
