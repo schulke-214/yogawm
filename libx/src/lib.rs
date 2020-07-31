47952dce70f65a049edf0ed6411adeaf29398320
schulke-214 <hello@maximilianschulke.com>
2020-08-01 01:24:49 +0200

    add a connect function

--- libx/src/lib.rs
+++ libx/src/lib.rs
@@ -1,10 +1,16 @@
 // this lib will contain easy bindings for creating, moving and closing x windows
 // probably using the following crate: https://docs.rs/x11rb/0.5.0/x11rb/
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub mod error;
+pub use error::X11Error;
+
+use x11rb::connection::Connection;
+
+pub type X11DisplayConnection = usize;
+pub type X11DisplayScreenNum = usize;
+pub type X11Result<T> = Result<T, X11Error>;
+
+pub fn connect() -> X11Result<(impl Connection + Sync + Send, X11DisplayScreenNum)> {
+    Ok(x11rb::connect(None)?)
 }
+
