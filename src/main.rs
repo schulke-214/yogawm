ee01892a57412dfa497b56aea4a4be4e76ae425b
schulke-214 <hello@maximilianschulke.com>
2020-08-01 01:08:31 +0200

    add example code for using libx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use libx::connect;
+use libx::X11Error;
+
+mod error;
+
+use error::YogaError;
+
+fn main() -> Result<(), YogaError> {
+    let x = connect()?;
+
+    println!("Hello, world! Screen ID {:#?}", x);
+
+    Ok(())
 }
