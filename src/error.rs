bf9190d9750fa52bb0fb627a1a70b710cd629b84
schulke-214 <hello@maximilianschulke.com>
2020-08-01 01:07:18 +0200

    add a global yoga error type

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+use libx::X11Error;
+
+#[derive(Debug)]
+pub enum YogaError {
+    X11Error(X11Error)
+}
+
+impl From<X11Error> for YogaError {
+    fn from(error: X11Error) -> Self {
+        YogaError::X11Error(error)
+    }
+}
+
